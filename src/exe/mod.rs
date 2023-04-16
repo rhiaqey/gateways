54541758bbd82b74dbab85067a9177d7e04ec378
dimitrmo <dmouratidis153@gmail.com>
2023-04-16 17:32:35 +0300

    Support encrypted data

--- src/exe/mod.rs
+++ src/exe/mod.rs
@@ -35,22 +35,15 @@ pub async fn run<
 
     let mut plugin = P::default();
     let port = executor.get_private_port();
-    let settings = executor.read_settings::<S>().await;
-    if settings.is_none() {
-        warn!("settings could not be found");
-    } else {
-        info!("setting retrieved successfully")
-    }
+    let settings = executor.read_settings::<S>().await.unwrap_or(S::default());
 
     let config = GatewayConfig {
         port: executor.get_public_port(),
         host: None,
     };
 
-    let mut publisher_stream = match plugin.setup(config, settings) {
-        Err(error) => {
-            panic!("failed to setup gateway: {error}");
-        }
+    let mut publisher_stream = match plugin.setup(config, Some(settings)) {
+        Err(error) => panic!("failed to setup gateway: {error}"),
         Ok(sender) => sender,
     };
 
@@ -81,11 +74,12 @@ pub async fn run<
                                 executor.set_channels(channel_list.channels).await;
                                 TOTAL_CHANNELS.set(channel_count);
                             }
-                            RPCMessageData::UpdateSettings(value) => {
-                                if let Ok(settings) = value.decode::<S>() {
-                                    debug!("received request to update settings rpc {:?}", settings);
+                            RPCMessageData::UpdateSettings() => {
+                                if let Ok(settings) = executor.read_settings::<S>().await {
                                     plugin.set_settings(settings).await;
                                     trace!("settings updated correctly");
+                                } else {
+                                    warn!("failed to read settings");
                                 }
                             }
                             _ => {}
